8ee692782473fbbd8824b35d9c6d0f3fb4d79b44
namrata-baldaniya <namratabaldaniya01@gmail.com>
2025-06-28 11:28:20 +0530

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -1,44 +1,61 @@
 function nbSelect(selector, options = {}) {
-  const theme = options.theme || "default";
   const label = options.label || "Select";
-  const submenus = options.submenus || {}; // predefined submenus
+  const icon = options.icon || "fa-bars";
+  const submenus = options.submenus || {};
 
   $(selector).each(function () {
     const $select = $(this);
     if ($select.data("enhanced") === 1) return;
 
     const $wrapper = $(`
-      <div class="nb-dropdown nb-theme-${theme}">
+      <div class="nb-dropdown">
         <div class="nb-dropdown-toggle">
-          <i class="fa-solid ${options.icon || 'fa-bars'}"></i> ${label}
+          <i class="fa-solid ${icon}"></i> ${label}
           <i class="fa-solid fa-chevron-down"></i>
         </div>
         <ul class="nb-dropdown-menu"></ul>
       </div>
     `);
 
+    const $toggle = $wrapper.find(".nb-dropdown-toggle");
     const $menu = $wrapper.find(".nb-dropdown-menu");
+
+    // Custom style
+    $toggle.css({
+      backgroundColor: options.backgroundColor || "#fff",
+      color: options.textColor || "#333",
+      borderRadius: "10px",
+      padding: "14px 18px",
+      fontWeight: "500",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      cursor: "pointer",
+    });
 
     $select.find("option").each(function () {
       const $opt = $(this);
+      const value = $opt.val();
       const icon = $opt.data("icon") || "";
       const text = $opt.text();
-      const value = $opt.val();
       const submenuItems = submenus[value];
 
       if (Array.isArray(submenuItems)) {
         const $li = $('<li class="nb-has-submenu"></li>');
-
         const $menuItem = $(`
           <div class="nb-menu-item">
-            <div class="nb-left-part"><i class="fa-solid ${icon}"></i> ${text}</div>
+            <div class="nb-left-part">
+              <i class="fa-solid ${icon}"></i> ${text}
+            </div>
             <i class="fa-solid fa-chevron-right nb-submenu-arrow"></i>
           </div>
         `);
 
         const $subMenu = $('<ul class="nb-submenu"></ul>');
         submenuItems.forEach((item) => {
-          const $subLi = $(`<li><i class="fa-solid ${item.icon}"></i> ${item.text}</li>`);
+          const $subLi = $(
+            `<li><i class="fa-solid ${item.icon}"></i> ${item.text}</li>`
+          );
           $subMenu.append($subLi);
         });
 
@@ -54,25 +71,51 @@ function nbSelect(selector, options = {}) {
       }
     });
 
+    // Hover colors
+    const hoverColor = options.hoverColor || "#f5f7fa";
+    $menu
+      .on("mouseenter", "li", function () {
+        $(this).css("background-color", hoverColor);
+      })
+      .on("mouseleave", "li", function () {
+        $(this).css("background-color", "");
+      });
+
+    // Add to DOM
     $select.hide().after($wrapper);
     $select.data("enhanced", 1);
   });
 
-
-  // Open submenu on hover
-  $(document).on("mouseenter", ".nb-has-submenu", function () {
-    $(this).find(".nb-submenu").stop(true, true).slideDown(150);
-  }).on("mouseleave", ".nb-has-submenu", function () {
-    $(this).find(".nb-submenu").stop(true, true).slideUp(150);
-  });
+  // Open/close toggle
   $(document).on("click", ".nb-dropdown-toggle", function (e) {
-  e.stopPropagation();
-  const $dropdown = $(this).closest(".nb-dropdown");
-  $(".nb-dropdown").not($dropdown).removeClass("open");
-  $dropdown.toggleClass("open");
-});
+    e.stopPropagation();
+    const $dropdown = $(this).closest(".nb-dropdown");
+    $(".nb-dropdown").not($dropdown).removeClass("open");
+    $dropdown.toggleClass("open");
+  });
 
-$(document).on("click", function () {
-  $(".nb-dropdown").removeClass("open");
-});
+  $(document).on("click", function () {
+    $(".nb-dropdown").removeClass("open");
+  });
+
+  // Submenu logic
+  $(document)
+    .on("mouseenter", ".nb-has-submenu", function () {
+      $(this).find(".nb-submenu").stop(true, true).slideDown(150);
+    })
+    .on("mouseleave", ".nb-has-submenu", function () {
+      $(this).find(".nb-submenu").stop(true, true).slideUp(150);
+    });
+
+  // Dropdown toggle logic
+  $(document).on("click", ".nb-dropdown-toggle", function (e) {
+    e.stopPropagation();
+    const $dropdown = $(this).closest(".nb-dropdown");
+    $(".nb-dropdown").not($dropdown).removeClass("open");
+    $dropdown.toggleClass("open");
+  });
+
+  $(document).on("click", function () {
+    $(".nb-dropdown").removeClass("open");
+  });
 }
